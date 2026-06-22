ee7e05cb01d9f2e9a704235055866ec9bab40244
mukul11215 <113311776+mukul11215@users.noreply.github.com>
2026-06-22 13:57:37 -0400

    Add SignIn, SignUp, and ProtectedRoute components

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,14 +4,40 @@ import Restaurants from "./pages/Restaurants.jsx";
 import Chains from "./pages/Chains.jsx";
 import MenuItems from "./pages/MenuItems.jsx";
 import NoMatch from "./pages/NoMatch.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/restaurants" element={<Restaurants />} />
-      <Route path="/chains" element={<Chains />} />
-      <Route path="/menu-items" element={<MenuItems />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route
+        path="/restaurants"
+        element={
+          <ProtectedRoute>
+            <Restaurants />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/chains"
+        element={
+          <ProtectedRoute>
+            <Chains />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/menu-items"
+        element={
+          <ProtectedRoute>
+            <MenuItems />
+          </ProtectedRoute>
+        }
+      />
       <Route path="*" element={<NoMatch />} />
     </Routes>
   );
