921b04a868cf770724325df3f5d7e0f0654db3dc
AlekLewis1 <alekseilewis@gmail.com>
2026-06-28 14:22:14 -0400

    Update Restaurants.jsx

--- src/pages/Restaurants.jsx
+++ src/pages/Restaurants.jsx
@@ -1,3 +1,47 @@
+import {settings} from "../../config/config";
+import {useAuth} from "../../services/useAuth";
+import useAxios from "../../services/useAxios";
+import {NavLink} from "react-router-dom";
+
 export default function Restaurants() {
-  return <h1>Restaurants Page</h1>;
+
+  const {user} = useAuth();
+
+  const url = settings.baseApiUrl + "/restaurants";
+
+  const {
+    data: restaurants,
+    error,
+    isLoading
+  } = useAxios(url, "GET", { Authorization: "Bearer " + user.jwt });
+
+  if (isLoading) return <div>Loading restaurants...</div>;
+  if (error) return <div>Error loading restaurants.</div>;
+
+  return (
+    <>
+      <h1>Restaurants</h1>
+
+      <div className="main-content container">
+        {restaurants && restaurants.map((restaurant) => (
+          <div key={restaurant.id} className="restaurant-card">
+
+            <NavLink
+              to={`/restaurants/${restaurant.id}`}
+              className="restaurant-link"
+            >
+              {restaurant.name}
+            </NavLink>
+
+            <div>{restaurant.address}</div>
+
+            <div>
+              Chain: {restaurant.chain ? restaurant.chain.name : "Unknown"}
+            </div>
+
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
