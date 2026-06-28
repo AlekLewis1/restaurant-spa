614b7fa63d594548cc173a0d1cf3f88e387d1e90
mukul11215 <113311776+mukul11215@users.noreply.github.com>
2026-06-28 17:08:49 -0400

    Refactor Header component for improved structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,21 @@
 import { NavLink } from "react-router-dom";
 import { useAuth } from "../src/services/useAuth.jsx";
 import {Navbar, Nav, Container} from "react-bootstrap";
-import RestaurantChains from "../pages/restaurantChains/restaurantChains.jsx";
 
 const Header = () => {
   const {isAuthed, user} = useAuth();
-
   const className = ({ isActive }) =>
     isActive ? "text-white fw-bold" : "text-white";
-
   return (
     <header className="p-3 bg-dark text-white">
       <nav className="container d-flex gap-3 align-items-center">
         <NavLink to="/" className={className}>Home</NavLink>
-
         <NavLink to="/restaurantChains" className={className}>Restaurant Chains</NavLink>
-
+        <NavLink to="/amenities" className={className}>Amenities</NavLink>
         <div className="ms-auto d-flex gap-3 align-items-center">
           {isAuthed
             ? <NavLink to="/signout" className={className}>Sign Out</NavLink>
             : <NavLink to="/signin" className={className}>Sign In / Sign Up</NavLink>
-            <NavLink to="/amenities" className={className}>Amenities</NavLink>
           }
         </div>
       </nav>
@@ -31,4 +26,3 @@ const Header = () => {
   );
 };
 export default Header;
-
